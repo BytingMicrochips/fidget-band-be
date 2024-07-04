92383c47d8a739e94a5c14e9373eca61d67aadd6
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2024-07-04 12:16:08 +0100

    add uncomment lines in connection.js for hosting BE

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 const { password } = require("./db_password");
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "test";
 
@@ -11,16 +11,17 @@ let uri = `mongodb+srv://emm-admin:${password}@cluster0.kv6vcn1.mongodb.net`;
 const client = new MongoClient(uri);
 
 const dbConnection = (req, res) => {
-  return client.connect().then(() => {
+  return client.connect()
+    .then(() => {
     return client.db();
   });
 };
 
-// dbConnection();
-// const pool = new Pool();
+dbConnection();
+const pool = new Pool();
 
 module.exports = {
   ENV,
-  // pool,
+  pool,
   client
 };
